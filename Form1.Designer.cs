a5744617d8e820dcedb92c3f9b8b56759202a629
igoridze.reap@gmail.com <igoridze.reap@gmail.com>
2020-06-06 17:34:10 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tool = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.sheepCount = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.wolfCount = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.timerSheep = new System.Windows.Forms.Timer(this.components);
+            this.timerWolf = new System.Windows.Forms.Timer(this.components);
+            this.field = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timerUpdateField = new System.Windows.Forms.Timer(this.components);
+            this.tool.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.field.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tool
+            // 
+            this.tool.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tool.Controls.Add(this.panel3);
+            this.tool.Controls.Add(this.panel2);
+            this.tool.Controls.Add(this.panel1);
+            this.tool.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tool.Location = new System.Drawing.Point(0, 0);
+            this.tool.Name = "tool";
+            this.tool.Size = new System.Drawing.Size(1066, 74);
+            this.tool.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button3);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(200, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(664, 72);
+            this.panel3.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Maroon;
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(0, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(664, 72);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "GO";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Controls.Add(this.sheepCount);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 72);
+            this.panel2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::game.Properties.Resources.add;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(147, 11);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 50);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::game.Properties.Resources.sheep;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(11, 11);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // sheepCount
+            // 
+            this.sheepCount.AutoSize = true;
+            this.sheepCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.sheepCount.Location = new System.Drawing.Point(67, 23);
+            this.sheepCount.Name = "sheepCount";
+            this.sheepCount.Size = new System.Drawing.Size(39, 29);
+            this.sheepCount.TabIndex = 1;
+            this.sheepCount.Text = "x0";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.wolfCount);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(864, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 72);
+            this.panel1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::game.Properties.Resources.add;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(18, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 50);
+            this.button1.TabIndex = 4;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // wolfCount
+            // 
+            this.wolfCount.AutoSize = true;
+            this.wolfCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.wolfCount.Location = new System.Drawing.Point(134, 23);
+            this.wolfCount.Name = "wolfCount";
+            this.wolfCount.Size = new System.Drawing.Size(39, 29);
+            this.wolfCount.TabIndex = 3;
+            this.wolfCount.Text = "x0";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::game.Properties.Resources.wolf;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(78, 11);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // timerSheep
+            // 
+            this.timerSheep.Interval = 500;
+            this.timerSheep.Tick += new System.EventHandler(this.timerSheep_Tick);
+            // 
+            // timerWolf
+            // 
+            this.timerWolf.Interval = 500;
+            this.timerWolf.Tick += new System.EventHandler(this.timerWolf_Tick);
+            // 
+            // field
+            // 
+            this.field.BackgroundImage = global::game.Properties.Resources.grass4;
+            this.field.Controls.Add(this.label1);
+            this.field.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.field.Location = new System.Drawing.Point(0, 74);
+            this.field.Name = "field";
+            this.field.Size = new System.Drawing.Size(1066, 387);
+            this.field.TabIndex = 1;
+            this.field.Paint += new System.Windows.Forms.PaintEventHandler(this.field_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, -20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            // 
+            // timerUpdateField
+            // 
+            this.timerUpdateField.Enabled = true;
+            this.timerUpdateField.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1066, 461);
+            this.Controls.Add(this.field);
+            this.Controls.Add(this.tool);
+            this.Name = "Form1";
+            this.Text = "Wolf&Sheeps";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tool.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.field.ResumeLayout(false);
+            this.field.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel tool;
+        private System.Windows.Forms.Panel field;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label sheepCount;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label wolfCount;
+        private System.Windows.Forms.Timer timerSheep;
+        private System.Windows.Forms.Timer timerWolf;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timerUpdateField;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,347 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace game
+{
+    
+
+
+
+    public partial class Form1 : Form
+    {
+
+        public class CAnimal
+        {
+            public int Left { get; set; }
+            public int Top { get; set; }
+            public int Width { get; set; }
+            public int Height { get; set; }
+            public Image BackgroundImage { get; set; }
+
+        }
+
+        public List<CAnimal> sheeps = new List<CAnimal>();
+        public List<CAnimal> wolfs = new List<CAnimal>();
+        public Random rnd = new Random();
+        public int step = 96;
+        public const int sizeAnimal = 48;
+        public int countAllSheeps = 0;
+        public int countAllWolfs = 0;
+
+        public int sheepTimePeriod = 300;
+        public int wolfTimePeriod = 400;
+
+        public Bitmap bitmap;
+      
+        public Form1()
+        {
+            InitializeComponent();
+          
+        }
+
+
+
+        public void newSheep()
+        {
+            int x = rnd.Next(0, field.Width);
+            int y = rnd.Next(0, field.Height);
+
+            if (x <= 0) x = 0;
+            if (x >= field.Width - sizeAnimal) x = field.Width - sizeAnimal;
+            if (y <= 0) y = 0;
+            if (y >= field.Height - sizeAnimal) y = field.Height - sizeAnimal;
+
+            var sheep = new CAnimal();
+            sheep.BackgroundImage = Properties.Resources.sheep;
+            
+            sheep.Width = 48;
+            sheep.Height = 48;
+            sheep.Left = x;
+            sheep.Top = y;
+            sheeps.Add(sheep);
+            //field.Controls.Add(sheep);
+            countAllSheeps++;
+            sheepCount.Text = "x" + countAllSheeps.ToString();
+        }
+
+        public void newWolf()
+        {
+            int x = rnd.Next(0, field.Width);
+            int y = rnd.Next(0, field.Height);
+
+            if (x <= 0) x = 0;
+            if (x >= field.Width - sizeAnimal) x = field.Width - sizeAnimal;
+            if (y <= 0) y = 0;
+            if (y >= field.Height - sizeAnimal) y = field.Height - sizeAnimal;
+
+            var wolf = new CAnimal();
+            wolf.BackgroundImage = Properties.Resources.wolf;
+           
+            wolf.Width = 48;
+            wolf.Height = 48;
+            wolf.Left = x;
+            wolf.Top = y;
+            wolfs.Add(wolf);
+           // field.Controls.Add(wolf);
+            countAllWolfs++;
+            wolfCount.Text = "x" + countAllWolfs.ToString();
+        }
+
+        public void calcPosition(CAnimal animal, int dir, int step)
+        {
+            switch(dir)
+            {
+                case 1:
+                    {
+                        animal.Top -= step;
+                        if (animal.Top <= 0) animal.Top = 0;
+                        break;
+                    }
+                case 2:
+                    {
+                        animal.Top -= step;
+                        if (animal.Top <= 0) animal.Top = 0;
+                        animal.Left += step;
+                        if (animal.Left >= field.Width-sizeAnimal) animal.Left = field.Width - sizeAnimal;
+                        break;
+                    }
+                case 3:
+                    {
+                        animal.Left += step;
+                        if (animal.Left >= field.Width - sizeAnimal) animal.Left = field.Width - sizeAnimal;
+                        break;
+                    }
+                case 4:
+                    {
+                        animal.Top += step;
+                        if (animal.Top >= field.Height-sizeAnimal) animal.Top = field.Height - sizeAnimal;
+                        animal.Left += step;
+                        if (animal.Left >= field.Width - sizeAnimal) animal.Left = field.Width - sizeAnimal;
+                        break;
+                    }
+                case 5:
+                    {
+                        animal.Top += step;
+                        if (animal.Top >= field.Height - sizeAnimal) animal.Top = field.Height - sizeAnimal;
+                        break;
+                    }
+                case 6:
+                    {
+                        animal.Top += step;
+                        if (animal.Top >= field.Height - sizeAnimal) animal.Top = field.Height - sizeAnimal;
+                        animal.Left -= step;
+                        if (animal.Left <= 0) animal.Left = 0;
+                        break;
+                    }
+                case 7:
+                    {
+                        animal.Left -= step;
+                        if (animal.Left <= 0) animal.Left = 0;
+                        break;
+                    }
+                case 8:
+                    {
+                        animal.Top -= step;
+                        if (animal.Top <= 0) animal.Top = 0;
+                        animal.Left -= step;
+                        if (animal.Left <= 0) animal.Left = 0;
+                        break;
+                    }
+            }
+        }
+        public void updateSheepsPosition()
+        {
+
+            for (int i = 0; i < sheeps.Count; i++)
+            {
+                int dir = rnd.Next(1, 9);
+                calcPosition(sheeps[i], dir, step);
+                Graphics gr = Graphics.FromImage(bitmap);
+                gr.DrawImage(sheeps[i].BackgroundImage, new Point(sheeps[i].Left, sheeps[i].Top));
+                field.Invalidate();
+            }
+        }
+
+        public void updateWolfsPosition()
+        {
+           
+            for (int i = 0; i < wolfs.Count; i++)
+            {
+                int dir = rnd.Next(1, 9);
+                calcPosition(wolfs[i], dir, step);
+ 
+                Graphics gr = Graphics.FromImage(bitmap);
+                gr.DrawImage(wolfs[i].BackgroundImage, new Point(wolfs[i].Left, wolfs[i].Top));
+                field.Invalidate();
+            }
+        }
+
+        public bool isIntersect(CAnimal a1, CAnimal a2)
+        {
+            return (a2.Left < a1.Left + a1.Width) &&
+            (a1.Left < (a2.Left + a2.Width)) &&
+            (a2.Top < a1.Top + a1.Height) &&
+            (a1.Top < a2.Top + a2.Height);
+        }
+            public void intersectSheepsAndSheeps()
+        {
+            int countAllSheeps = sheeps.Count;
+
+            for (int i = 0; i<countAllSheeps-1; i++)
+            {
+                
+                for (int j=i+1; j <countAllSheeps; j++)
+                {
+        
+                    
+                    if (isIntersect(sheeps[i], sheeps[j]))
+                    {
+                        newSheep();
+                    }
+                }
+            }
+        }
+
+        public void intersectWolfsAndSheeps()
+        {
+
+            foreach (var wolf in wolfs)
+            {
+              for (int i = 0; i < countAllSheeps; i++)
+                {
+                    if (isIntersect(wolf, sheeps[i]))
+                    {
+                        sheeps[i].BackgroundImage = Properties.Resources.icons8_crossbones_64; // когда овечка умирает- появляется картинка костей
+                        sheeps.Remove(sheeps[i]);
+                        countAllSheeps--;
+                        sheepCount.Text = "x" + countAllSheeps.ToString();
+                        break;
+                    }
+               }
+            }
+        }
+
+        public void updateSheepSituation()
+        {
+                updateSheepsPosition();
+                intersectSheepsAndSheeps();
+
+        }
+        public void updateWolfSituation()
+        {
+            updateWolfsPosition();
+            intersectWolfsAndSheeps();
+        }
+
+        public void initAnimals()
+        {
+            wolfs.Clear();
+            sheeps.Clear();
+            countAllSheeps = 0;
+            countAllWolfs = 0;
+           
+            newWolf();
+            newSheep();
+            newSheep();
+
+            updateSheepsPosition();
+            updateWolfsPosition();
+            field.Invalidate();
+
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            typeof(Panel).InvokeMember("DoubleBuffered", BindingFlags.SetProperty | BindingFlags.Instance | BindingFlags.NonPublic, null, field, new object[] { true });
+
+            bitmap = new Bitmap(field.Width, field.Height);
+            initAnimals();
+/*
+            TimerCallback tmSheep = new TimerCallback(updateSheepSituation);
+            System.Threading.Timer timerSheep = new System.Threading.Timer(tmSheep, 0, 0, 500);
+           
+            TimerCallback tmWolf = new TimerCallback(updateWolfSituation);
+            System.Threading.Timer timerWolf = new System.Threading.Timer(tmWolf, 0, 0, 500);
+*/
+        }
+
+        private void timerSheep_Tick(object sender, EventArgs e)
+        {
+            updateSheepSituation();
+         }
+
+        private void timerWolf_Tick(object sender, EventArgs e)
+        {
+            updateWolfSituation();
+ 
+            if (countAllSheeps <= 0)
+            {
+                timerSheep.Enabled = false;
+                timerWolf.Enabled = false;
+                DialogResult result = MessageBox.Show("Do you want restart game?", "Game Over", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                if (result == DialogResult.Yes)
+                {
+                    initAnimals();
+                    field.Invalidate();
+                }
+            }
+
+            if (countAllSheeps >= 500)
+            {
+                timerSheep.Enabled = false;
+                timerWolf.Enabled = false;
+                DialogResult result = MessageBox.Show("Wolves can't eat so many sheep! \n Do you want restart game?", "Game Over", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                if (result == DialogResult.Yes)
+                {
+                    initAnimals();
+                    field.Invalidate();
+                }
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            newWolf();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            newSheep();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timerSheep.Enabled = true;
+            timerWolf.Enabled = true;
+        }
+
+        private void field_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics gr = e.Graphics;
+
+           gr.DrawImage(bitmap, 0, 0, field.Width, field.Height);
+            //bitmap = new Bitmap(field.Width, field.Height);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            bitmap = new Bitmap(field.Width, field.Height);
+        }
+
+        private void scroolSheepSpeed_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+    }
+}
